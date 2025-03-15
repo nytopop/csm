6f7121946737de10cf7e41dddd314584cf455fbd
nytopop <ericizoita@gmail.com>
2025-03-15 15:04:35 -0700

    not good demo
    - ripped out watermarking crap for perf reasons
    - torch compile the 100m decoder for perf reasons
    - wrote a janky fastrtc streaming demo
    
    ???

--- generator.py
+++ generator.py
@@ -1,15 +1,15 @@
 from dataclasses import dataclass
 from typing import List, Tuple
 
+import time
 import torch
 import torchaudio
+from contextlib import ExitStack
 from huggingface_hub import hf_hub_download
 from models import Model, ModelArgs
 from moshi.models import loaders
 from tokenizers.processors import TemplateProcessing
 from transformers import AutoTokenizer
-from watermarking import CSM_1B_GH_WATERMARK, load_watermarker, watermark
-
 
 @dataclass
 class Segment:
@@ -51,8 +51,6 @@ class Generator:
         mimi = loaders.get_mimi(mimi_weight, device=device)
         mimi.set_num_codebooks(32)
         self._audio_tokenizer = mimi
-
-        self._watermarker = load_watermarker(device=device)
 
         self.sample_rate = mimi.sample_rate
         self.device = device
@@ -104,6 +102,63 @@ class Generator:
         return torch.cat([text_tokens, audio_tokens], dim=0), torch.cat([text_masks, audio_masks], dim=0)
 
     @torch.inference_mode()
+    def generate_streaming(
+        self,
+        text: str,
+        speaker: int,
+        context: List[Segment],
+        max_audio_length_ms: float = 10_000,
+        temperature: float = 0.9,
+        topk: int = 50,
+    ):
+        self._model.reset_caches()
+
+        max_audio_frames = int(max_audio_length_ms / 80)
+        tokens, tokens_mask = [], []
+        for segment in context:
+            segment_tokens, segment_tokens_mask = self._tokenize_segment(segment)
+            tokens.append(segment_tokens)
+            tokens_mask.append(segment_tokens_mask)
+
+        gen_segment_tokens, gen_segment_tokens_mask = self._tokenize_text_segment(text, speaker)
+        tokens.append(gen_segment_tokens)
+        tokens_mask.append(gen_segment_tokens_mask)
+
+        prompt_tokens = torch.cat(tokens, dim=0).long().to(self.device)
+        prompt_tokens_mask = torch.cat(tokens_mask, dim=0).bool().to(self.device)
+
+        curr_tokens = prompt_tokens.unsqueeze(0)
+        curr_tokens_mask = prompt_tokens_mask.unsqueeze(0)
+        curr_pos = torch.arange(0, prompt_tokens.size(0)).unsqueeze(0).long().to(self.device)
+
+        max_seq_len = 2048 - max_audio_frames
+        if curr_tokens.size(1) >= max_seq_len:
+            raise ValueError(f"Inputs too long, must be below max_seq_len - max_audio_frames: {max_seq_len}")
+
+        self._audio_tokenizer._start_streaming(1, ExitStack())
+
+        for _ in range(max_audio_frames):
+            sample = self._model.generate_frame(curr_tokens, curr_tokens_mask, curr_pos, temperature, topk)
+            if torch.all(sample == 0):
+                break  # eos
+
+            frame = self._audio_tokenizer.decode(torch.stack([sample]).permute(1, 2, 0)).squeeze(0).squeeze(0)
+
+            curr_tokens = torch.cat([sample, torch.zeros(1, 1).long().to(self.device)], dim=1).unsqueeze(1)
+            curr_tokens_mask = torch.cat(
+                [torch.ones_like(sample).bool(), torch.zeros(1, 1).bool().to(self.device)], dim=1
+            ).unsqueeze(1)
+            curr_pos = curr_pos[:, -1:] + 1
+
+            yield frame, sample
+
+        self._audio_tokenizer._stop_streaming()
+
+    @torch.inference_mode()
+    def decode_samples(self, samples):
+        return self._audio_tokenizer.decode(torch.stack(samples).permute(1, 2, 0)).squeeze(0).squeeze(0)
+
+    @torch.inference_mode()
     def generate(
         self,
         text: str,
@@ -153,13 +208,6 @@ class Generator:
 
         audio = self._audio_tokenizer.decode(torch.stack(samples).permute(1, 2, 0)).squeeze(0).squeeze(0)
 
-        # This applies an imperceptible watermark to identify audio as AI-generated.
-        # Watermarking ensures transparency, dissuades misuse, and enables traceability.
-        # Please be a responsible AI citizen and keep the watermarking in place.
-        # If using CSM 1B in another application, use your own private key and keep it secret.
-        audio, wm_sample_rate = watermark(self._watermarker, audio, self.sample_rate, CSM_1B_GH_WATERMARK)
-        audio = torchaudio.functional.resample(audio, orig_freq=wm_sample_rate, new_freq=self.sample_rate)
-
         return audio
 
 
@@ -172,8 +220,10 @@ def load_csm_1b(ckpt_path: str = "ckpt.pt", device: str = "cuda") -> Generator:
         audio_num_codebooks=32,
     )
     model = Model(model_args).to(device=device, dtype=torch.bfloat16)
-    state_dict = torch.load(ckpt_path)
+    state_dict = torch.load(ckpt_path, weights_only=True)
     model.load_state_dict(state_dict)
+    model.decoder = torch.compile(model.decoder, fullgraph=True, mode='reduce-overhead')
 
-    generator = Generator(model)
-    return generator
+    gen = Generator(model)
+
+    return gen
